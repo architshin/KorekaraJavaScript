43e0d548675bcebfce239706728770a985f1eac5
Shinzo SAITO <architshin@websarva.com>
2018-02-14 01:49:35 +0900

    サンプルの追加。

---
+++ chap12/attributeAddEvent.js
@@ -0,0 +1,18 @@
+//［すべて…］チェックボックスをクリックしたときの処理関数。
+function onCheckallChanged() {
+	//［すべて…］チェックボックス要素を取得。
+	let checkall = document.getElementById("checkall");
+	//［すべて…］のチェック状態に合わせて表示文字列を変更。
+	if(checkall.checked) {
+		checkall.nextSibling.textContent = "すべて外す";
+	} else {
+		checkall.nextSibling.textContent = "すべてチェック";		
+	}
+	//name属性がskillのチェックボックス要素を取得。
+	let checkboxes = document.getElementsByName("skill");
+	//name属性がskillのチェックボックス要素をループ処理。
+	for(let checkbox of checkboxes) {
+		//各チェックボックスのチェック状態を［すべて…］に合わせる。
+		checkbox.checked = checkall.checked;
+	}
+}
