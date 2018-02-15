da89a162621a2afe2bba58922af68b0a56b7b058
Shinzo SAITO <architshin@websarva.com>
2018-02-15 16:02:24 +0900

    ステップ1部分のコメントを記述。

--- chap13/BMI.js
+++ chap13/BMI.js
@@ -1,14 +1,20 @@
+//BMI計算オブジェクト
 class BMI {
+	//コンストラクタ。引数は身長と体重。
 	constructor(height = 0, weight = 0) {
 		this.height = height;
 		this.weight = weight;
 	}
 
+	//BMIを計算するメソッド。
 	getBMI() {
+		//BMIを計算。
 		let bmi = this.weight / Math.pow(this.height/100, 2);
+		//以下３行で小数第2位を四捨五入して小数点以下一桁表示に。
 		bmi = bmi * 10;
 		bmi = Math.round(bmi);
 		bmi = bmi / 10;
+		//BMIをリターン。
 		return bmi;
 	}
 	
