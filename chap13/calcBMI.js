460e0fa10c175899aae4b0726d659442c3c6fe22
Shinzo SAITO <architshin@websarva.com>
2018-02-16 21:01:42 +0900

    ステップ2部分のコメントを記述。

--- chap13/calcBMI.js
+++ chap13/calcBMI.js
@@ -13,15 +13,22 @@ function onCalcBMIButtonClick() {
 	let bmiObj = new BMI(height, weight);
 	//BMIを取得。
 	let bmi = bmiObj.getBMI();
+
 	//BMIを表示するspan要素を取得。
 	let bmiResult = document.getElementById("bmiResult");
 	//BMIを表示。
 	bmiResult.textContent = bmi;
 
+	//アドバイスを取得。
 	let advice = bmiObj.getAdvice();
+	//アドバイスを表示するspan要素を取得。
 	let adviceMessage = document.getElementById("adviceMessage");
+	//アドバイスを表示。
 	adviceMessage.textContent = advice;
+
+	//結果を表示するp要素を取得。
 	let resultMessage = document.getElementById("resultMessage");
+	//非表示にしているクラス属性を削除。
 	resultMessage.removeAttribute("class");
 }
 
