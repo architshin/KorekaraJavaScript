027d8329bf606c99af254313944553e50fc9febb
Shinzo SAITO <architshin@websarva.com>
2018-02-28 15:24:02 +0900

    Chap9を本脱稿に合わせて修正。

--- chap09/accessor.js
+++ chap09/accessor.js
@@ -6,11 +6,6 @@ class AccessorProp {
 		this.lastName = lastName;
 		this.firstName = firstName;
 	}
-
-	//プロパティの氏名を引数の文字列で結合した文字列を生成するメソッド。
-	getConcatName(space = " ") {
-		return this.lastName + space + this.firstName;
-	}
 	
 	//nameプロパティのゲッタ。
 	get name() {
@@ -26,20 +21,21 @@ class AccessorProp {
 
 //「中田太郎」という氏名でAccessorPropオブジェクトを生成。
 let taro = new AccessorProp("中田", "太郎");
-//メソッドgetConcatName()で氏名結合文字列を取得し、表示。引数は初期値を使用。
-let getConcatNameAns = taro.getConcatName();
-console.log("getConcatName:" + getConcatNameAns);
-//concatNameプロパティを表示。
-console.log("concatName:" + taro.concatName);
+//各プロパティを表示。
+console.log("lastName: " + taro.lastName);
+console.log("firstName: " + taro.firstName);
 //nameプロパティを表示。
 console.log("name: " + taro.name);
 
-//nameプロパティに「山口次郎」を代入したうえで同プロパティを表示。
-taro.name = "山口次郎";
-console.log("name: " + taro.name);
-
-//extNameプロパティに「山口次郎」を代入したうえでextNameプロパティを表示。
+//extNameプロパティに「山口次郎」を代入。
 taro.extName = "山口次郎";
+//各プロパティを表示。
+console.log("lastName: " + taro.lastName);
+console.log("firstName: " + taro.firstName);
+
+//extNameプロパティを表示。
 console.log("extName: " + taro.extName);
-//さらにnameプロパティを表示。
+
+//nameプロパティに「篠原三郎」を代入したうえで同プロパティを表示。
+taro.name = "篠原三郎";
 console.log("name: " + taro.name);
