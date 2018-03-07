bc2b0ceb68fb0b14988834524375560025d0d42a
Shinzo SAITO <architshin@websarva.com>
2018-03-08 00:59:35 +0900

    Chap11をさらに修正。

--- chap11/addAndRemoveElements.js
+++ chap11/addAndRemoveElements.js
@@ -10,8 +10,8 @@ function onAddListItemButtonClick() {
 	let listItem = document.createElement("li");
 	//li要素のテキスト部分に入力された文字列を設定。
 	listItem.textContent = addListItemInputText;
-	//class属性にblueTextを設定。
-	listItem.className = "blueText";
+	//class属性にblueTextを追加。
+	listItem.classList.add("blueText");
 	//リスト表示要素を取得。
 	let skillList = document.getElementById("skillList");
 	//リスト表示の末尾に生成したli要素を追加。
--- chap11/editAttribute.js
+++ chap11/editAttribute.js
@@ -4,15 +4,15 @@
 function onP2RedButtonClick() {
 	//idがshowTextのp要素を取得。
 	let showText = document.getElementById("showText");
-	//取得したp要素のstyle属性に文字色を赤にするように設定。
-	// showText.setAttribute("style", "color: red");
-	showText.style = "color: red";
+	//取得したp要素のclass属性にredTextクラスを設定。
+	showText.setAttribute("class", "redText");
+	// showText.className = "redText";
 }
 
 //［赤文字を元通りに］ボタンクリック時の処理。
 function onP2DefaultButtonClick() {
 	//idがshowTextのp要素を取得。
 	let showText = document.getElementById("showText");
-	//取得したp要素のstyle属性をを削除。
-	showText.removeAttribute("style");
+	//取得したp要素のclass属性をを削除。
+	showText.removeAttribute("class");
 }
